5934475c41e884fe03ef56a32a1a5cb11bfc4211
minyu <minyu@outlook.com>
2026-03-19 00:52:13 +0800

    补上mini.hipatterns配置

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-mini/mini.hipatterns",
+    opts = {
+      highlighters = {
+        -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+        fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+        hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+        todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+        note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+        -- Highlight hex color strings (`#rrggbb`) using that color
+        hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
+      },
+    },
+  },
+}
